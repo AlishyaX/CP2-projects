7c24c79b56232bfa97af8a34331e35fcec5c6140
AlishyaX <alishya.xavier@ucas-edu.net>
2025-01-08 13:44:15 -0700

    started steps

--- financial calculater/main.py
+++ financial calculater/main.py
@@ -1,4 +1,29 @@
 # Alishya Xavier, Financial Calculater
-#alishya
+
+'''
+Create a program that completes the following basic financial calculations:
+How long it will take to save for a goal based on a weekly or monthly deposit
+Compound Interest Calculator 
+Budget Allocator (use set percentages to divide an income into spending categories like savings, entertainment, and food)
+Sale Price Calculator (apply discounts to prices)
+Tip Calculator
+
+PROJECT STEPS:
+Create a main function that will run the user interface
+Using project decomposition to figure out what other functions you need and how they interact with each other
+Create your functions
+Have at least 2 people test your code to make sure it works
+'''
+
+def save_goal():
+    pass
+def c_i_calc():
+    pass
+def budget_alloc():
+    pass
+def s_p_calc():
+    pass
+def tip_calc():
+    pass
 def main():
     pass
