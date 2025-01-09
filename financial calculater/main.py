7475adc14c308369f0e28328a0689128c64b2349
AlishyaX <alishya.xavier@ucas-edu.net>
2025-01-09 13:44:42 -0700

    worked on project

--- financial calculater/main.py
+++ financial calculater/main.py
@@ -14,16 +14,45 @@ Using project decomposition to figure out what other functions you need and how 
 Create your functions
 Have at least 2 people test your code to make sure it works
 '''
+while True:
 
-def save_goal():
-    pass
-def c_i_calc():
-    pass
-def budget_alloc():
-    pass
-def s_p_calc():
-    pass
-def tip_calc():
-    pass
-def main():
-    pass
+    def save_goal():
+        #Finds what their goal is
+        #Finds what there monthly or weekly deposits are 
+        goal = int(input('What is your goal for how much you want to save:\n'))
+        monthorweek = input('Are you going to give deposits weekly or monthly?\n')
+        if monthorweek == 'weekly':
+            payw = input('')
+        elif monthorweek == 'monthly':
+        else:
+            print('That is not an option')
+
+        pass
+    def c_i_calc():
+        pass
+    def budget_alloc():
+        pass
+    def s_p_calc():
+        pass
+    def tip_calc():
+        pass
+    def main():
+        #User interface
+        #Calls all other functions
+        print('Welcome to the basic financial calculator!\n')
+        pick = input('What would you like to do?\n1. See how long it will take to save for a goal based on a weekly or monthly deposit\n2. Compound Interest Calculator\n3. Budget Allocator\n4. Sale Price Calculator\n5. Tip Calculator\n')
+        if pick == '1':
+            save_goal()
+        elif pick == '2':
+            c_i_calc()
+        elif pick == '3':
+            budget_alloc()
+        elif pick == '4':
+            s_p_calc()
+        elif pick == '5':
+            tip_calc()
+        else:
+            print('That is not one of the options')
+            break
+
+    main()
