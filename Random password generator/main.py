5db772ce00806d5a796b5732907d236541d94e22
AlishyaX <alishya.xavier@ucas-edu.net>
2025-01-27 21:07:10 -0700

    fixed random password generator to be able to meet rquirements

--- Random password generator/main.py
+++ Random password generator/main.py
@@ -8,35 +8,70 @@ import string
 
 #This is the function for the length of each password
 def length():
-    return int(input("How long do you want your password: "))
+    while True:
+        user_password_length = input("What is the length of your password: ")
+        if user_password_length.isdigit():
+            return int(user_password_length)
+        else:
+            print('That is not an option')
+            
 
 
 #This is the function to use uppercase letters in the password
 def uppercase():
-    return input("Do you want to use uppercase letters? (y/n): ").lower() == 'y'
-
+    while True:
+        user_uppercase_letters = input("Do you want to use uppercase letters? (y/n): ").lower()
+        if user_uppercase_letters == 'y':
+            return True
+        elif user_uppercase_letters == 'n':
+            return False
+        else:
+            print('That is not an option')
+    
 
 #This is the function to use lower case letters in the password
 def lowercase():
-    return input("Do you want to use lowercase letters? (y/n): ").lower() == 'y'
+    while True:
+        user_lowercase_letters = input("Do you want to use lowercase letters? (y/n): ").lower()
+        if user_lowercase_letters == 'y':
+            return True
+        elif user_lowercase_letters == 'n':
+            return False
+        else:
+            print('That is not an option')
+    
 
 
 #This is the function to use numbers in the password
 def num():
-    return input("Do you want to use numbers? (y/n): ").lower() == 'y'
-
+    while True:
+        user_num = input("Do you want to use numbers? (y/n): ").lower()
+        if user_num == 'y':
+            return True
+        elif user_num == 'n':
+            return False
+        else:
+            print('That is not an option')
 
 #This is the function to use special characters in the password
 def special_char():
-    return input("Do you want to use special characters? (y/n): ").lower() == 'y'
+    while True:
+        user_special_character = input("Do you want to use special characters? (y/n): ").lower()
+        if user_special_character == 'y':
+            return True
+        elif user_special_character == 'n':
+            return False
+        else:
+            print('That is not an option')
+    
 
 '''
-This is the main function that uses ASCII letters to be able to not 
+This is the function that uses ASCII letters to be able to not 
 have to make a seperate list with all of the options but instead
 just get to pull it from a pre-made one that python already recognizes
 '''
 
-def main(length, upper, lower, digits, special):
+def data_collection(length, upper, lower, digits, special):
     char = ''
     if upper:
         char += string.ascii_uppercase
@@ -46,25 +81,35 @@ def main(length, upper, lower, digits, special):
         char += string.digits
     if special:
         char += string.punctuation
-    
+
     password = ''.join(random.choice(char) for x in range(length))
     return password
 
 
-# These variables are used in the key function to be able to print all of the questions together
-len = length()
-upper = uppercase()
-lower = lowercase()
-numbers = num()
-special_characters = special_char()
-
 
 # This function gathers the information from the variables to then be able to make 4 different passwords with the requirements
 def key():
-    passwords = [main(len, upper, lower, numbers, special_characters) for x in range(4)]
+    len = length()
+    upper = uppercase()
+    lower = lowercase()
+    numbers = num()
+    special_characters = special_char()
+    passwords = [data_collection(len, upper, lower, numbers, special_characters) for x in range(4)]
     for i, password in enumerate(passwords, 1):
         print(f"Password {i}: {password}")
 
 
-#This is the start of the program by calling the key function
-key()
+#This is the main function that handles user interface by asking the user what they want to do
+def main():
+    while True:
+        option = input('What do you want to do:\n1. Make 4 passwords\n2. Exit\n')
+        if option == '1':
+            key()
+        elif option == '2':
+            print('Have a good day! :)')
+            break
+        else:
+            print('That is not one of the options')
+
+#This is the start of the program by calling the main function
+main()
